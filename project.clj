6c76282b75fbadeaea3ececd417f4aa3babb5b6f
Danny Wilson <danny@prime.vc>
2015-01-22 14:07:01 +0100

    Update ring. Fix a bunch of lein deps warnings.

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
 (defproject prime/ring-middleware "1.0.0-SNAPSHOT"
   :description      "Ring middleware and configuration shared between our different web apps."
 
-  :dependencies [ [org.clojure/clojure "1.5.1"]
-                  
-                  [lib-noir "0.2.0"]
-                  [containium "0.1.0-SNAPSHOT" :exclusions [boxure/clojure]]
-                  [ring "1.2.2" :exclusions [ring/ring-jetty-adapter]]
+  :dependencies [ 
+                  [lib-noir "0.2.0" :exclusions [ring cheshire org.clojure/clojure]]
+                  [containium "0.1.0-SNAPSHOT" :exclusions [boxure clojurewerkz/elastisch com.sonian/elasticsearch-zookeeper http-kit org.apache.kafka/kafka_2.9.2 org.clojure/tools.reader org.elasticsearch/elasticsearch jline org.apache.httpcomponents/httpclient lein-light-nrepl info.sunng/ring-jetty9-adapter com.maitria/packthread com.draines/postal cc.qbits/alia boxure/clojure org.clojure/core.async org.apache.cassandra/cassandra-all]]
+                  [ring/ring-core  "1.3.2" :exclusions [org.clojure/tools.reader org.clojure/clojure]]
+                  [ring/ring-devel "1.3.2"]
                   [prone "0.4.0"]
                 ]
 
